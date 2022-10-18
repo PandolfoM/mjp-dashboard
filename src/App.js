ec7eef31552abff252a4c055dd71c53f357a719c
Matthew Pandolfo <matt@pandolfo.com>
2022-10-18 10:10:21 -0400

    render map function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import config from "./config.json";
+
 function App() {
+  let services = config.services;
+
   return (
     <div className="App">
       <header>MJP Dashboard</header>
+      <ul>
+        {services.map((item, i) => (
+          <li key={i}>
+            <p>{item.name}</p>
+            {item.items.map((item, i) => (
+              <ul key={i}>
+                <a href={item.url}>{item.name}</a>
+              </ul>
+            ))}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
