dcf0ed65cf5b2b7a9493b318f83f75a7b4801810
Matthew Pandolfo <matt@pandolfo.com>
2022-10-19 09:06:15 -0400

    create tooltip

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
+import Tooltip from "./Tooltip";
 function Header() {
   const [date, setDate] = useState(new Date());
 
   setInterval(() => {
-    setDate(new Date())
-  }, 1 * 1000)
+    setDate(new Date());
+  }, 1 * 1000);
 
   return (
     <div className="m-5">
@@ -15,7 +18,14 @@ function Header() {
           {date.toLocaleTimeString()}
         </h2>
       </div>
-      <hr className="border-slate-600"/>
+      <hr className="border-slate-600" />
+      <div className="m-5 flex justify-end">
+        <Tooltip tooltipText={"Notepad"} orientation="bottom">
+          <a href="/">
+            <FontAwesomeIcon icon={faNoteSticky} inverse size="2x" />
+          </a>
+        </Tooltip>
+      </div>
     </div>
   );
 }
