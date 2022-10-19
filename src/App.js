37b85e36b946eb706fe5c194652d310c6312fc29
Matthew Pandolfo <matt@pandolfo.com>
2022-10-18 20:57:33 -0400

    divider

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ function App() {
       <div className="flex flex-wrap justify-start gap-5">
         {services.map((item, i) => (
           <div className="w-full md:w-auto flex-auto" key={i}>
-            <h3 className="text-3xl m-5 font-semibold text-white">
+            <h3 className="text-3xl m-5 mb-1 font-semibold text-white">
               {item.name}
             </h3>
+            
             {item.items.map((item, i) => (
               <a
-                className="hover:shadow-[5px_5px_5px_indigo] hover:scale-105 transition-all ease-linear duration-300 m-5 p-6 max-w-2xl bg-slate-700 rounded-xl shadow-lg flex items-center space-x-4 border border-solid border-gray-600"
+                className="hover:-translate-y-1.5 transition-all ease-linear duration-200 m-5 p-6 max-w-2xl bg-slate-700 rounded-xl shadow-lg flex items-center space-x-4 border border-solid border-slate-600"
                 href={item.url}
                 target={"_blank"}
                 rel="noreferrer"
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
-  const [date, setDate] =useState(new Date())
+  const [date, setDate] = useState(new Date());
 
   setInterval(() => {
     setDate(new Date())
   }, 1 * 1000)
 
-  
   return (
-    <div className="flex justify-between m-5">
-      <h1 className="text-3xl font-bold text-white">MJP Dashboard</h1>
-      <h2 className="text-2xl font-semibold text-white">{date.toLocaleTimeString()}</h2>
+    <div className="m-5">
+      <div className="flex justify-between mb-2">
+        <h1 className="text-3xl font-bold text-white">MJP Dashboard</h1>
+        <h2 className="text-2xl font-semibold text-white">
+          {date.toLocaleTimeString()}
+        </h2>
+      </div>
+      <hr className="border-slate-600"/>
     </div>
   );
 }
